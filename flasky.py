f5f722e29ae25d26108d8344a616939dc97ec40a
strawluffy <cui_zz@126.com>
2017-12-18 17:44:53 +0800

    单文件的最后一个版本

--- flasky.py
+++ flasky.py
@@ -9,9 +9,9 @@ from wtforms.validators import InputRequired
 ## 插件
 from flask_moment import Moment
 from flask_bootstrap import Bootstrap
-from flask_script import Manager
+from flask_script import Manager,Shell
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate,MigrateCommand
 
 class myform(FlaskForm):
     username = StringField(u'用户名',validators=[InputRequired()])
@@ -20,13 +20,10 @@ class myform(FlaskForm):
 
 ## 数据库表格
 
-
-
-
-
 myflask = Flask(__name__)
 myflask.config['SECRET_KEY'] = '123456'
-myflask.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://flasktest:Flask1204.@172.18.26.210/flasktest'
+myflask.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://flasktest:Flask1204.@172.18.26.210/flasktest?charset=utf8'
+myflask.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
 # myflask.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\AIT-003\\Envs\\flask\\project\\db\\test.db'
 Bootstrap(myflask)
 Moment(myflask)
@@ -39,9 +36,8 @@ class Role(db.Model):
     name = db.Column(db.String(64),unique = True)
     users = db.relationship('User',backref = 'role')
 
-
     def __repr__(self):
-        return '< Role {} >'.format(self.name)
+        return u'< Role {} >'.format(self.name)
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -49,25 +45,41 @@ class User(db.Model):
     username = db.Column(db.String(64),unique = True, index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     def __repr__(self):
-        return '< User {} >'.format(self.username)
-
-
-
-
-
+        return u'< User {} >'.format(self.username)
 
 @myflask.route('/',methods=['GET','POST'])
+
+
+def make_shell_context():
+    return dict(app=myflask,db=db,User=User,Role=Role)
+manage.add_command("shell",Shell(make_context=make_shell_context))
+
+migrate = Migrate(myflask,db)
+manage.add_command('db',MigrateCommand)
+
+
 def index():
     form = myform()
     if form.validate_on_submit():
-        if not form.username.data == '111':
-            flash(u'需要输入一些内容')
+        # if not form.username.data == '111':
+            # flash(u'需要输入一些内容')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None:
+            session['known'] = False
+            user = User(username = form.username.data)
+            db.session.add(user)
+            db.session.commit()
+        else:
+            session['known'] = True
+
         session['username'] = form.username.data
         session['password'] = form.password.data
+        form.username.data = ''
+        form.password.data = ''
         return redirect(url_for('index'))
     return render_template('index.html',current_time = datetime.utcnow(),
                             username = session.get('username'),password = session.get('password'),
-                            form = form)
+                            form = form,known = session.get('known'))
 
 if __name__ == '__main__':
     manage.run()
